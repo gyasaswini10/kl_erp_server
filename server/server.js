c3df70db862e4e7e12807ca2551ba3c29a3e667e
YASASWINI <gyasu1110@gmail.com>
2024-11-12 16:23:56 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -15,6 +15,6 @@ const express = require('express');
 	app.use('/api/users', userRoutes);
 
 	const PORT = process.env.PORT || 5000;
-	app.listen(PORT, () => {
-  	console.log(`Server running on port ${PORT}`);
-	});
+	app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
